187265b6acf896a7b2e0eadfeffa3259a2bcbe00
mobit <mobit@gmail.com>
2018-02-09 15:42:44 +0530

    createMessage and newmeassage

--- server/server.js
+++ server/server.js
@@ -1,16 +1,47 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env || 3000;
+const port = process.env.PORT || 3000;
 //console.log(publicPath);
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user conncted');
+
+    // socket.emit('newEmail', {
+    //     from: 'example@gmail.com',
+    //     text: 'hey whats going on.',
+    //     createdAt: 123
+    // });
+
+    // socket.on('createEmail' ,(cEmail) => {
+    //     console.log('create a new email', cEmail);
+    // });
+
+    socket.on('createMessage', (message) => {
+        console.log('new message', message)
+    });
+
+    socket.on( 'disconnect' ,() => {
+        console.log('user was disconnected');
+    });
+
+   socket.emit('newMessage', {
+    from: 'prasoon',
+    text: 'hello prasoon',
+    createAt: 123
+   });
+});
  
-app.listen(port, () => {
+server.listen(port, () => {
 
  console.log(`server up to port ${port}`);
 
