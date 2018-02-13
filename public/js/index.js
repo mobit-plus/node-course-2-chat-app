864dc85b5d5553a6bcc0f7a5a30d6f3babfcc09d
mobit <mobit@gmail.com>
2018-02-13 12:15:30 +0530

    mustache library

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,22 @@
 
 var socket = io();
 
+    function scrollToBottom () {
+        var messages = jQuery('#message');
+        var newMessage = messages.children('li:last-child');
+
+        var clientHeight = messages.prop('clientHeight');
+        var scrollTop = messages.prop('scrollTop');
+        var scrollHeight = messages.prop('scrollHeight');
+        var newMessageHeight = newMessage.innerHeight;
+        var lastMessageHeight = newMessage.prev().innerHeight();
+
+        if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+            console.log('scrolling');
+            messages.scrollTop(scrollHeight);
+        }
+    };
+
         socket.on('connect',function () {
             console.log('new user connected');
 
@@ -25,23 +41,43 @@ var socket = io();
         });
 
         socket.on('newMessage', function (message) {
-            var formattedTime = moment(message.createdAt).format('h:mm a');
-            //console.log('New-Message', message);
-            var li = jQuery('<li></li>');
+            var formattedTime = moment(message.createdAt).format('MMM Do YYYY h:mm a');
+            var template = jQuery('#message-template').html();
+            var html = Mustache.render(template,{
+                text: message.text,
+                from: message.from,
+                createdAt: formattedTime
+            });
 
-            li.text(`${message.from} ${formattedTime} :${message.text}`);
-            jQuery('#message').append(li);
+            jQuery('#message').append(html);
+            scrollToBottom();
+
+            // var formattedTime = moment(message.createdAt).format('h:mm a');
+            // //console.log('New-Message', message);
+            // var li = jQuery('<li></li>');
+
+            // li.text(`${message.from} ${formattedTime} :${message.text}`);
+            // jQuery('#message').append(li);
         });
 
         socket.on('newLocationMessage', function (message) {
-            var formattedTime = moment(message.createdAt).format('h:mm a');
-            var li = jQuery('<li></li>');
-            var a = jQuery('<a target="_blank">My Current Location</a>');
+            var formattedTime = moment(message.createdAt).format('MMM Do YYYY h:mm a');
+            var location_template = jQuery('#location-message-template').html();
+                var html = Mustache.render(location_template, {
+                    from: message.from,
+                    url: message.url,
+                    createdAt: formattedTime
+                })
+                jQuery('#message').append(html);
+                scrollToBottom();
+            // var formattedTime = moment(message.createdAt).format('h:mm a');
+            // var li = jQuery('<li></li>');
+            // var a = jQuery('<a target="_blank">My Current Location</a>');
 
-            li.text(`${message.from}${formattedTime}:`);
-            a.attr('href', message.url);
-            li.append(a);
-            jQuery('#message').append(li);
+            // li.text(`${message.from}${formattedTime}:`);
+            // a.attr('href', message.url);
+            // li.append(a);
+            // jQuery('#message').append(li);
         });
 
         // socket.on('joined', function (join) {
@@ -60,17 +96,18 @@ var socket = io();
         //     console.log('Acknowledgment from server:- Data is got it!');
         // });
 
-        jQuery('#message-form').on('submit', function (event) {
-            event.preventDefault();
+        jQuery('#message-form').on('submit', function (e) {
+            e.preventDefault();
 
             var messageText = jQuery('[name="message"]');
 
             socket.emit('createMessage',{
-                from: 'prasoon',
+                from: 'Prasoon',
                 text: messageText.val()
             }, function () {
                 messageText.val();
             });
+            scrollToBottom();
         });
         var locationButton = jQuery('#send-location');
 
@@ -93,7 +130,7 @@ var socket = io();
                 locationButton.removeAttr('disabled').text('Send location');
                 alert('unable to fetch location');
             });
-            
+            scrollToBottom();
         });
 
         
